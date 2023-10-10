dbecfcaa78e5f8c75d3a78bf0bf2d5cd72db197b
Mainque Bautista Menjoulou <mmenjoulou@alumnos.exa.unicen.edu.ar>
2023-10-09 21:47:56 -0300

    Started with new purchase back-end

---
+++ Assets/Scripts/PurchasePanelController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PurchasePanelController : MonoBehaviour
+{
+    public GameObject purchasesContent;
+    public TMP_InputField dayInput;//dd
+    public TMP_InputField monthInput;//mm
+    public TMP_InputField yearInput;//yyyy
+    public GameObject suppliersDropdown;
+    public GameObject productContent;
+
+    private void Start()
+    {
+
+    }
+
+}
